180e1528b5aa1413fa22a358f49d5f608c71cba9
Gamearoo Codes <gamearoo10@gmail.com>
2021-12-28 13:23:23 -0500

    ep7

--- src/commands/Fun/8ball.js
+++ src/commands/Fun/8ball.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
-const { apiversion } = require("../../../config");
+const { api8ball } = require("ram-api.js");
+const { apiversion, ramapiversion, ramapikey } = require("../../../config");
 
 module.exports = {
 	name: "8ball",
@@ -12,28 +13,12 @@ module.exports = {
 				content: "Please ask a 3 or more word question!",
 			});
 
-		await axios
-			.get(`/8ball`, { baseURL: `https://ram.gamearoo.top/api/${apiversion}` })
-			.then(async function (response) {
-				if (response.data.Too_many_requests)
-					return message.reply({
-						content: response.data.Too_many_requests,
-						allowedMentions: { repliedUser: false },
-					});
-				let text = await response.data.text;
-				//error it by adding .json() ^
-				message.reply({
-					content: text,
-					allowedMentions: { repliedUser: false },
-				});
-			})
-			.catch((error) => {
-				console.log(error);
-
-				message.reply({
-					content: `There was a error connecting to the ram api ${apiversion} ${error.response.statusText}`,
-					allowedMentions: { repliedUser: false },
-				});
+		await api8ball(ramapiversion, ramapikey).then((data) => {
+			//error it by adding .json() ^
+			message.reply({
+				content: data.text,
+				allowedMentions: { repliedUser: false },
 			});
+		});
 	},
 };
