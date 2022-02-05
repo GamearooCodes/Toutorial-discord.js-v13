493cce4b7e82cba7f42045ac47fd79500aa6ce30
Gamearoo Codes <gamearoo10@gmail.com>
2022-02-05 16:58:53 -0500

    ep8

--- src/commands/Fun/8ball.js
+++ src/commands/Fun/8ball.js
@@ -1,24 +1,28 @@
 const axios = require("axios");
-const { api8ball } = require("ram-api.js");
-const { apiversion, ramapiversion, ramapikey } = require("../../../config");
+const { api8ball, executeconsole } = require("ram-api.js");
+const { ramapiversion } = require("../../../config");
+const { ramapikey } = require("../../../secure/token");
 
 module.exports = {
 	name: "8ball",
-	desc: "ask the bot a question",
+	desc: `Ask the bot a question`,
 	category: "Fun",
-	usage: "8ball",
+	usage: "8balll [question]",
 	async execute(client, message, args, MessageEmbed) {
 		if (!args[3])
 			return message.reply({
-				content: "Please ask a 3 or more word question!",
+				content: "Please ask a 3 word or more question!",
 			});
 
-		await api8ball(ramapiversion, ramapikey).then((data) => {
-			//error it by adding .json() ^
-			message.reply({
-				content: data.text,
-				allowedMentions: { repliedUser: false },
+		await api8ball(ramapiversion, ramapikey)
+			.then((data) => {
+				message.reply({
+					content: data.text,
+					allowedMentions: { repliedUser: false },
+				});
+			})
+			.catch((err) => {
+				executeconsole(err, true, false);
 			});
-		});
 	},
 };
